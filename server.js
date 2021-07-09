d771c772b39b3c4005adde0125979ea54adc23e7
Praneeth Soddum <61727627+pr365@users.noreply.github.com>
2021-07-08 23:42:30 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require("express"),
+    http = require("http"),
+    app = express(),
+    toDos = [
+        { 
+            "description" : "Get groceries",
+            "tags"  : [ "shopping", "chores" ]
+        },
+        { 
+            "description" : "Make up some new ToDos",
+            "tags"  : [ "writing", "work" ]
+        },
+        {
+            "description" : "Prep for Monday's class",
+            "tags"  : [ "work", "teaching" ]
+        },
+        { 
+            "description" : "Answer emails",
+            "tags"  : [ "work" ]
+        },
+        { 
+            "description" : "Take Gracie to the park",
+            "tags"  : [ "chores", "pets" ]
+        },
+        { 
+            "description" : "Finish writing this book",
+            "tags"  : [ "writing", "work" ]
+        }
+    ]
+        
+app.use(express.static(__dirname + "/client"));
+
+app.use(express.urlencoded());
+
+http.createServer(app).listen(7000);
+
+app.get("/todos.json", function (req, res) {
+    res.json(toDos);
+});
+
+app.post("/todos", function (req, res) {
+    var newToDo = req.body;
+
+    console.log(newToDo);
+
+    toDos.push(newToDo);
+
+    res.json({"message":"You posted to the server!"});
+});
